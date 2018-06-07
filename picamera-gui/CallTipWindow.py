adc67d2047b0153d06a188524c7baa9e0b517b68
Jérémy Talbot-Pâquet <jtpaquet@gmail.com>
2018-06-07 15:07:08 -0400

    Ajout de usr bin env python

--- picamera-gui/CallTipWindow.py
+++ picamera-gui/CallTipWindow.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 # http://www.voidspace.org.uk/python/weblog/arch_d7_2006_07_01.shtml
--- picamera-gui/Exceptions_ModuleCamera.py
+++ picamera-gui/Exceptions_ModuleCamera.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 # Ce fichier contient deux classes d'exceptions qui héritent de "Exception"
